5b83d6672d5ce9c330072f46073e9321317445e0
nguyendangkhang79 <nguyendangkheng@gmail.com>
2026-09-25 18:05:13 +0700

    Xóa code thừa

--- backend/src/main/resources/password-flow-migration.sql
+++
@@ -1,5 +0,0 @@
--- Migration script for password flow feature
--- Chạy script này trên cơ sở dữ liệu MySQL: fashion_system
-ALTER TABLE nguoi_dung
-    ADD COLUMN must_change_password BOOLEAN NOT NULL DEFAULT FALSE;
-
